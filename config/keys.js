cdce110a9a9749ca3f1ea46d0e4bb3cae24e9bc2
Gabe Ewusk <gabeewsuk@gmail.com>
2021-04-29 12:19:39 -0600

    connecting to mongo

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI:
-      "mongodb+srv://Grabes:SaberSlice120@cluster0.dusdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      "mongodb+srv://Grabes:CIZiBuePZEGuG44H@cluster0.dusdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   };
   
