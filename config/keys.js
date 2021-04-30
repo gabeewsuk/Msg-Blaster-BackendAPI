8b7ca10a5a1f6ada24775d649b95e935b90ef2bc
Gabe Ewusk <gabeewsuk@gmail.com>
2021-04-30 12:19:04 -0600

    changed quotes in mongoURI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI:
-      "mongodb+srv://Grabes:CIZiBuePZEGuG44H@cluster0.dusdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      'mongodb+srv://Grabes:CIZiBuePZEGuG44H@cluster0.dusdl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   };
   
