ef76ea2ddcbbabb9da987de951dc56aa9faed7b1
Gabe Ewusk <gabeewsuk@gmail.com>
2021-05-04 19:30:49 -0600

    removed getData func

--- routes/api/Items.js
+++ routes/api/Items.js
@@ -10,7 +10,7 @@ const Item = require("../../models/Items");
 // @description Get All Items
 // @access Public
 
-router.get("/", getData, (req, res) => {
+router.get("/", (req, res) => {
   Item.find()
     .sort({ date: 1 })
     .then((items) => res.json(items));
