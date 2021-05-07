a1a27132e973eb3aad574f934864b3eeaf52e5b9
Gabe Ewusk <gabeewsuk@gmail.com>
2021-05-06 22:47:52 -0600

    added cors

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use("/api/Items", Items);
 app.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Allow-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Allow-Control-Allow-Headers', 'Content-Type');
+  res.header('Allow-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 
 
